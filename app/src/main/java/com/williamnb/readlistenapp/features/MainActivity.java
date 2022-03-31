9f28d344e5b999360ddd6d1608f53e5c7293becc
William <williamnb.vn@gmail.com>
2022-03-31 18:27:49 +0700

    RLA-20: BottomNavigationView + NavigationCompose

--- app/src/main/java/com/williamnb/readlistenapp/features/MainActivity.java
+++ app/src/main/java/com/williamnb/readlistenapp/features/MainActivity.java
@@ -1,15 +1,10 @@
 package com.williamnb.readlistenapp.features;
 
-import android.os.Bundle;
-import android.view.View;
-
-import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.williamnb.readlistenapp.R;
 import com.williamnb.readlistenapp.base.BaseActivity;
 import com.williamnb.readlistenapp.databinding.ActivityMainBinding;
@@ -28,7 +23,10 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainViewMode
 
     @Override
     public void initializeView() {
-
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(viewBinding.bottomNav, navController);
     }
 
     @Override
@@ -47,17 +45,9 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainViewMode
     }
 }
 
-
-/*
- **  BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
- **  NavController navController = Navigation.findNavController(this,  R.id.nav_host_fragment);
- **  NavigationUI.setupWithNavController(bottomNavigationView, navController);
- * */
-
-    /*
-    * Hi I know this is a 4 month old comment at the time but I also had this issue (can't click the other icons with this code) and managed to fix it. I thought if anyone else has a problem I will clarify how I fixed it for myself:
-You must name your menu items with the EXACT same id as your fragment id. I don't think it's mentioned in the video, but whatever the ids are on your fragments, they must match on the menu items as well or it won't navigate.
-Cheers and I hope you found your solution!
-*
-* https://www.youtube.com/watch?v=Chso6xrJ6aU&ab_channel=Stevdza-San
-    * */
+/** - BottomNavigationView + NavigationCompose: --> src:
+** NavHostFragment navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment);
+** NavController navController = navHostFragment.getNavController();
+** BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+** NavigationUI.setupWithNavController(bottomNav, navController);
+**/
