3c88bfb3f24920974ac7ef9500d730a8b0b76a3b
William <williamnb.vn@gmail.com>
2022-04-07 19:37:40 +0700

    VinApp-04: Sign out

--- app/src/main/java/com/williamnb/readlistenapp/features/chat/ChatMainFragment.java
+++ app/src/main/java/com/williamnb/readlistenapp/features/chat/ChatMainFragment.java
@@ -1,14 +1,30 @@
 package com.williamnb.readlistenapp.features.chat;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.williamnb.readlistenapp.base.BaseFragment;
 import com.williamnb.readlistenapp.databinding.FragmentChatMainBinding;
+import com.williamnb.readlistenapp.prefs.PreferenceManager;
+import com.williamnb.readlistenapp.utilities.Constants;
+
+import java.util.HashMap;
 
 public class ChatMainFragment extends BaseFragment<FragmentChatMainBinding, ChatMainViewModel> {
+
+    private PreferenceManager preferenceManager;
+
     @Override
     public FragmentChatMainBinding createViewBinding(LayoutInflater inflater, ViewGroup container) {
         return FragmentChatMainBinding.inflate(inflater, container, false);
@@ -22,6 +38,7 @@ public class ChatMainFragment extends BaseFragment<FragmentChatMainBinding, Chat
     @Override
     public void initializeView() {
         hideBottomNavigationView(true);
+        preferenceManager = new PreferenceManager(getContext());
     }
 
     @Override
@@ -31,11 +48,55 @@ public class ChatMainFragment extends BaseFragment<FragmentChatMainBinding, Chat
 
     @Override
     public void initializeEvents() {
-
+        viewBinding.imageSignOut.setOnClickListener(view -> signOut());
     }
 
     @Override
     public void initializeData() {
+        loadUserDetails();
+        getToken();
+    }
 
+    private void loadUserDetails() {
+        viewBinding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        viewBinding.imageProfile.setImageBitmap(bitmap);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getToken() {
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+
+    private void updateToken(String token) {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                database.collection(Constants.KEY_COLLECTION_USERS).document(
+                        preferenceManager.getString(Constants.KEY_USER_ID)
+                );
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused -> showToast("Token đã cập nhật thành công"))
+                .addOnFailureListener(e -> showToast("Không thể cập nhật token"));
+    }
+
+    private void signOut() {
+        showToast("Đang đăng xuất...");
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                database.collection(Constants.KEY_COLLECTION_USERS).document(
+                        preferenceManager.getString(Constants.KEY_USER_ID)
+                );
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(unused -> {
+                    preferenceManager.clear();
+
+                })
+                .addOnFailureListener(e -> showToast("Không thể đăng xuất"));
     }
 }
--- app/src/main/java/com/williamnb/readlistenapp/features/chat/ChatMainViewModel.java
+++ app/src/main/java/com/williamnb/readlistenapp/features/chat/ChatMainViewModel.java
@@ -3,4 +3,5 @@ package com.williamnb.readlistenapp.features.chat;
 import com.williamnb.readlistenapp.base.BaseViewModel;
 
 public class ChatMainViewModel extends BaseViewModel {
+
 }
