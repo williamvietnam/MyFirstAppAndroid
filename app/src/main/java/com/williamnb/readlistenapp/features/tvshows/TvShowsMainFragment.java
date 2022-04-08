a6766145c57716a84071d647405cb1c2619cd9fb
William <williamnb.vn@gmail.com>
2022-04-09 00:57:53 +0700

    VinApp-09: Setup tvShows (continue)

---
+++ app/src/main/java/com/williamnb/readlistenapp/features/tvshows/TvShowsMainFragment.java
@@ -0,0 +1,4 @@
+package com.williamnb.readlistenapp.features.tvshows;
+
+public class TvShowsMainFragment {
+}
---
+++ app/src/main/java/com/williamnb/readlistenapp/network/ApiClient.java
@@ -0,0 +1,7 @@
+package com.williamnb.readlistenapp.network;
+
+import retrofit2.Retrofit;
+
+public class ApiClient {
+    private static Retrofit retrofit;
+}
